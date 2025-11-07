d2dd1ac107f562255f304c8d44ffc8ab373b9b16
25thOliver <os679736@gmail.com>
2025-11-07 10:06:22 +0300

    Correct the requirements parsing

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 # Read requirements
 with open(os.path.join(this_directory, "requirements.txt"), encoding="utf-8") as f:
-    requirements = [line.strip() for line in f if line.strip() and not not line.startswith("#")]
+    requirements = [line.strip() for line in f if line.strip() and not line.startswith("#")]
 
 setup(
     name="sami-datagen",
